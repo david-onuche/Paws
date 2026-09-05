104b2e7cd60b3d6beadc3b89e7f52c77886d5762
davidonuche <iamdavidonuche@gmail.com>
2026-09-05 13:59:24 +0100

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by David Onuche on 05/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
