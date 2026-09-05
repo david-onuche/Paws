063ffbe9cd82342f853a691890e398b36aa27442
davidonuche <iamdavidonuche@gmail.com>
2026-09-05 22:58:06 +0100

    Update on Data Sample.

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -12,11 +12,49 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query private var pets: [Pet]
+    
+    let layout = [
+        GridItem(.flexible(minimum: 120)),
+        GridItem(.flexible(minimum: 120)),
+    ]
+    
     var body: some View {
         NavigationStack {
             ScrollView {
-                EmptyView()
-            }
+                LazyVGrid(columns: layout) {
+                    GridRow {
+                        ForEach(pets) { pet in
+                            NavigationLink(destination: EmptyView()) {
+                                VStack {
+                                    if let imageDate = pet.photo {
+                                      if let uiImage = UIImage(data: imageDate) {
+                                            Image(uiImage: uiImage)
+                                        }
+                                    } else{
+                                        Image(systemName: "pawprint.circle")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .padding(40)
+                                            .foregroundStyle(.quaternary)
+                                    }
+                                    Spacer()
+                                    Text(pet.name)
+                                        .font(.title.weight(.light))
+                                        .padding(.vertical)
+                                    
+                                    Spacer()
+                                } //: VSTACK
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                            } //: NAVLINK
+                            .foregroundStyle(.primary)
+                        } //: LOOP
+                    } //: GRID ROW
+                } //: GRID LAYOUT
+                .padding(.horizontal)
+            } //: SCROLLVIEW
+            .navigationTitle(pets.isEmpty ? "" : "Paws")
             .overlay{
                 if pets.isEmpty {
                     CustomContentUnavailableView(
@@ -28,6 +66,11 @@ struct ContentView: View {
             }
         }
     }
+}
+
+#Preview("Sample Data") {
+    ContentView()
+        .modelContainer(Pet.preview)
 }
 
 #Preview("No Data") {
